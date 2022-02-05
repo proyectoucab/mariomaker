b8e8b3de4a748e9e4bca705114364febef569e3e
proyectoucab <wddiaz.20@est.ucab.edu.ve>
2022-02-05 11:25:13 -0400

    Archivo Musica y CustomFileOpener

--- src/AePlayWave.java
+++ src/AePlayWave.java
@@ -8,48 +8,48 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 /**
- * Esta clase se usa para reproducir sonidos en el juego. 
- * 
+ * Esta clase se usa para reproducir sonidos en el juego. Esto crea un nuevo subproceso para reproducir el sonido.
  *
  */
 public class AePlayWave{
-
-    static AePlayWave fondoMusica;
-private SourceDataLine linea = null;
+private SourceDataLine line = null;
 private byte[] audioBytes;
 	private int numBytes;
 	private AudioFormat audioFormat;
 	private DataLine.Info info;
-        public static Object fondito;
+        static AePlayWave fondoMusica; // musica de fondo del juego
 
-	
+	/**
+	 * Abre el archivo especificado por la ruta y lo carga en la memoria para que pueda reproducirse
+	 */
 	public AePlayWave(String path){
 		File soundFile = new File(path);
 		AudioInputStream audioInputStream = null;
 		try{
 			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
 		}catch (Exception ex){
-			
+                    
 		}
 		if(audioInputStream == null)return;
 		audioFormat = audioInputStream.getFormat();
-		info = new DataLine.Info(SourceDataLine.class,
-								 audioFormat);
+		info = new DataLine.Info(SourceDataLine.class,audioFormat);
 		try{
-			linea = (SourceDataLine)AudioSystem.getLine(info);
-			linea.open(audioFormat);
+			line = (SourceDataLine)AudioSystem.getLine(info);
+			line.open(audioFormat);
 		}catch (LineUnavailableException ex){
-			
+			System.out.println("*** no se ha leido la linea de musica ***");
+			System.exit(1);
 		}
 
-		linea.start();
+		line.start();
 
 		audioBytes = new byte[(int)soundFile.length()];
 
 		try{
 			numBytes = audioInputStream.read(audioBytes, 0, audioBytes.length);
 		}catch (IOException ex){
-			
+			System.out.println("*** no se pudo leer " + soundFile + " ***");
+			System.exit(1);
 		}
 	}
 	
@@ -58,23 +58,21 @@ private byte[] audioBytes;
 	 */
 	public void start(){
 		new Thread(){
-                       
 			public void run(){
 				try{
-					linea = (SourceDataLine)AudioSystem.getLine(info);
-					linea.open(audioFormat);
+					line = (SourceDataLine)AudioSystem.getLine(info);
+					line.open(audioFormat);
 				}catch (LineUnavailableException ex){
-					//System.out.println("*** Audio linea unavailable ***");
-					//System.exit(1);
+					System.out.println("*** Audio linea no disponible ***");
+					System.exit(1);
 				}
-				linea.start();
-				linea.write(audioBytes, 0, numBytes);
+				line.start();
+				line.write(audioBytes, 0, numBytes);
 			}
 		}.start();
 	}
-        public void finalizar(){
-          
-			linea.close();
-                        
+        
+        public void finalizarMusica(){
+            line.close();
         }
 }
